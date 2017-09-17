cb1b49596caea48ee8606f51a1a4bf1d6a9652fc
Ilya Nurullin <nurullinin@mail.ru>
2017-09-17 09:36:29 +0500

    The first version of the solution

---
+++ Task8/GraphGenerator.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8
+{
+    class GraphGenerator
+    {
+        public const int edgeCount = 50;
+
+        public static List<Tuple<int, int>> WalkGeneration()
+        {
+            var rand = new Random();
+
+            var res = new List<Tuple<int, int>>();
+
+            int[,] Matrix = new int[edgeCount, edgeCount];
+
+            int currentEdge = 0;
+            int iterationCounter = 0;
+            while ((iterationCounter < 50 || rand.NextDouble() >= 0.1) && iterationCounter <= 100)
+            {
+                var freeEdges = new List<int>();
+
+                for (int i = 0; i < edgeCount - 1; i++)
+                {
+                    if (currentEdge == i)
+                        continue;
+                    
+                    if (Matrix[currentEdge, i] == 0)
+                        freeEdges.Add(i);
+                }
+
+                int nextEdge = freeEdges[rand.Next(freeEdges.Count - 1)];
+
+                AddVertex(ref Matrix, ref res, currentEdge, nextEdge);
+                currentEdge = nextEdge;
+                iterationCounter++;
+            }
+
+            if (currentEdge == 0)
+                return res;
+            else
+            {
+                if (Matrix[0, currentEdge] == 0)
+                {
+                    AddVertex(ref Matrix, ref res, currentEdge, 0);
+                    return res;
+                }
+                else
+                {
+                    AddVertex(ref Matrix, ref res, currentEdge, edgeCount - 1);
+                    AddVertex(ref Matrix, ref res, edgeCount - 1, 0);
+                    return res;
+                }
+            }
+
+        }
+
+        public static int Count(int[,] Matrix, int i)
+        {
+            int res = 0;
+
+            for (int j = 0; j < Matrix.GetLength(1); j++)
+            {
+                if (Matrix[i, j] == 1)
+                    res++;
+            }
+
+            return res;
+        }
+
+        static void AddVertex(ref int[,] Matrix, ref List<Tuple<int, int>> list, int a, int b)
+        {
+            Matrix[a, b] = 1;
+            Matrix[b, a] = 1;
+            list.Add(new Tuple<int, int>(a, b));
+        }
+       
+    }
+}
--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,15 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Task8
 {
     class Program
     {
+        private static int[,] Matrix;
+
         static void Main(string[] args)
         {
+
+            List<Tuple<int, int>> graph = new List<Tuple<int, int>>();
+
+            int edgeCount = GraphGenerator.edgeCount;
+            
+            graph = GraphGenerator.WalkGeneration();
+
+            Matrix = new int[edgeCount, edgeCount];
+            
+            ConverGraph(graph);
+
+            var res = new List<int>();
+
+            int currentEdge = 0;
+
+            var st = new Stack<int>();
+            st.Push(0);
+
+            while (st.Count != 0)
+            {
+                currentEdge = st.Peek();
+                if (GraphGenerator.Count(Matrix, currentEdge) == 0)
+                {
+                    res.Add(currentEdge);
+                    st.Pop();
+                }
+                else
+                {
+                    for (int i = Matrix.GetLength(1) - 1; i >= 0; i--)
+                    {
+                        if (Matrix[currentEdge, i] == 1)
+                        {
+                            RemoveVertice(currentEdge, i);
+                            st.Push(i);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            var resStr = "";
+            for (int i = 0; i < res.Count; i++)
+            {
+                resStr += res[i] + "->";
+            }
+            Console.WriteLine(resStr.TrimEnd('-','>'));
+        }
+
+
+        static void ConverGraph(List<Tuple<int, int>> graph)
+        {
+            foreach (var tuple in graph)
+            {
+                Matrix[tuple.Item1, tuple.Item2] = 1;
+                Matrix[tuple.Item2, tuple.Item1] = 1;
+            }
+        }
+
+        static void RemoveVertice(int e1, int e2)
+        {
+            Matrix[e1, e2] = 0;
+            Matrix[e2, e1] = 0;
         }
     }
 }
